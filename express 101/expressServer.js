1de222b8068a6b3c5922d3503cca38097c2e3303
Anthony Malary <amalary@bu.edu>
2022-08-11 17:02:36 -0700

    Express basics

--- express 101/expressServer.js
+++ express 101/expressServer.js
@@ -1,27 +1,59 @@
+// Node js is the language 
+// Express is Node, a node module more specifically 
+
+// http is a native module 
+const http = require('http')
+
+// Express is third party module 
+const express = require('express')
+
+// An "app" is the express function(createApplication inside the Express module)
+// Invoked and is an Express application 
+
+
+// All is a method, and it takes 2 args
+// 1.route 
+// 2.callback to run if the route is requested 
+const app = express()
+
+app.all('*', (req,res) =>{
+    // Express handles the basic headers (status code, mime-type) ! 
+    res.send("<h1>This is the home page</h1>")
+    // Express handles the end. 
+
+})
+
+const PORT = 3000
+
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`)
+})
 
 
 
 
-let array = [{name:'Nafim'},{name:'Anthony'},{name:'Vanessa'}]
-
-let unsubbed = array.map(fans => fans.name )
-
-console.log(unsubbed) 
-
-let set1 = new Set(unsubbed)
-
-console.log(set1.has('Vanessa'))
 
 
+
+// unsubbed = []
+
+// let array = [{name:'Nafim'},{name:'Anthony'},{name:'Vanessa'}]
 
 
 // for(let i = 0; i < array.length; i++){
-
+    
 //     unsubbed.push(array[i].name)
-
+    
 // }
+
+
 
 // console.log(unsubbed)
 
+// let unsubbed = array.map(fans => fans.name )
 
+// console.log(unsubbed) 
 
+// let set1 = new Set(unsubbed)
+
+// console.log(set1.has('Vanessa'))
--- express 101/nodeServer.js
+++ express 101/nodeServer.js
@@ -35,7 +35,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200,{'content-type': 'image/png'})
         const image = fs.readFileSync('Node.js_logo.svg.png')
         res.write(image)
-        res.end();
+        res.end()
 
 
     }else if(req.url == '/styles.css'){
@@ -43,11 +43,12 @@ const server = http.createServer((req, res) => {
         res.writeHead(200,{'content-type': 'text/css'})
         const css = fs.readFileSync('styles.css')
         res.write(css)
-        res.end();
+        res.end()
 
         
 
     }else{
+        
         res.writeHead(404,{'content-type': 'text/html'})
         res.write('<h4>This is the wrong page </h4>')
         res.end()
